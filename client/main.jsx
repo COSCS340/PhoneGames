25374b314f43877b643e4fc7ec44fb11250ca5a4
Jerry (Jeremiah) Duncan <mikeynjerry@gmail.com>
2018-03-01 23:03:33 -0500

    Temporary create/new game pages

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { Session } from 'meteor/session';
 import App from '../imports/ui/App.jsx';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
-Metoer.startup(() => {
-    render(<App />, document.getElementById('render-target'))
+
+
+Meteor.startup(() => {
+	Session.set("currentView", "homepage");
+    //render(<App />, document.getElementById('render-target'));
+});
+
+Template.homepage.events({
+	'click #btn-new-game': function () {
+		console.log("worked2");
+		Session.set("currentView", "newGame");
+	},
+	'click #btn-join-game': function () {
+		console.log("worked3");
+		Session.set("currentView", "joinGame");
+	}
+});
+
+Template.main.helpers({
+	currentView: function(){
+		return Session.get("currentView");
+	}
 });
